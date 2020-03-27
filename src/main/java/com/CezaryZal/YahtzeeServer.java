ff4256cf34077dcdf41016357adf68380f43ae7a
CezaryZal <czarek0821@gmail.com>
2020-03-27 22:08:08 +0100

    Create test for Four (Single) category

--- src/main/java/com/CezaryZal/YahtzeeServer.java
+++ src/main/java/com/CezaryZal/YahtzeeServer.java
@@ -1,8 +1,15 @@
 package com.CezaryZal;
 
+import java.util.List;
+
 public class YahtzeeServer {
 
-    public int getNumber() {
-        return 2;
+    public int getPoint(List<Integer> scoreOfThrows, String category) {
+        if (category.equals("Four")){
+            return scoreOfThrows.stream()
+                    .filter((a) -> a == 4)
+                    .reduce(0, Integer::sum);
+        }
+        return 0;
     }
 }
--- src/test/java/com/CezaryZal/YahtzeeServerTest.java
+++ src/test/java/com/CezaryZal/YahtzeeServerTest.java
@@ -2,15 +2,20 @@ package com.CezaryZal;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class YahtzeeServerTest {
 
     @Test
-    void testMethod(){
+    void shouldGetReceiveResultFromSingleCategoryForFourType(){
         YahtzeeServer yahtzeeServer = new YahtzeeServer();
-        int number = yahtzeeServer.getNumber();
-        assertThat(number).isEqualTo(2);
+        List<Integer> scoreOfThrows = Arrays.asList(1, 1, 2, 2, 4, 4);
+        String category = "Four";
+        int number = yahtzeeServer.getPoint(scoreOfThrows, category);
+        assertThat(number).isEqualTo(8);
     }
 
 }
