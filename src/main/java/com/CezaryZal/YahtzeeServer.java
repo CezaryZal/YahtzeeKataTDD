8b219d4c5993775c2870f0f2f8ea97b992182851
CezaryZal <czarek0821@gmail.com>
2020-03-28 20:40:05 +0100

    Create test for Three and Six Category;
    Change JDK version to 10;

--- src/main/java/com/CezaryZal/YahtzeeServer.java
+++ src/main/java/com/CezaryZal/YahtzeeServer.java
@@ -1,15 +1,24 @@
 package com.CezaryZal;
 
 import java.util.List;
+import java.util.Map;
 
 public class YahtzeeServer {
 
+    private final Map<String, Integer> pointTable = Map.of(
+            "One", 1,
+            "Two", 2,
+            "Three", 3,
+            "Four", 4,
+            "Five", 5,
+            "Six", 6);
+
     public int getPoint(List<Integer> scoreOfThrows, String category) {
-        if (category.equals("Four")){
-            return scoreOfThrows.stream()
-                    .filter((a) -> a == 4)
-                    .reduce(0, Integer::sum);
-        }
-        return 0;
+        Integer pointByCategory = pointTable.get(category);
+
+       return  scoreOfThrows.stream()
+                .filter((number) -> number.equals(pointByCategory))
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
--- src/test/java/com/CezaryZal/YahtzeeServerTest.java
+++ src/test/java/com/CezaryZal/YahtzeeServerTest.java
@@ -1,5 +1,6 @@
 package com.CezaryZal;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,13 +10,32 @@ import static org.assertj.core.api.Assertions.*;
 
 class YahtzeeServerTest {
 
+    YahtzeeServer yahtzeeServer;
+    @BeforeEach
+    void setUp() {
+        yahtzeeServer = new YahtzeeServer();
+    }
+
     @Test
     void shouldGetReceiveResultFromSingleCategoryForFourType(){
-        YahtzeeServer yahtzeeServer = new YahtzeeServer();
         List<Integer> scoreOfThrows = Arrays.asList(1, 1, 2, 2, 4, 4);
         String category = "Four";
+        int collectedPoints = yahtzeeServer.getPoint(scoreOfThrows, category);
+        assertThat(collectedPoints).isEqualTo(8);
+    }
+    @Test
+    void shouldGetReceiveResultFromSingleCategoryForThreeType(){
+        List<Integer> scoreOfThrows = Arrays.asList(3, 3, 3, 2, 4, 4);
+        String category = "Three";
         int number = yahtzeeServer.getPoint(scoreOfThrows, category);
-        assertThat(number).isEqualTo(8);
+        assertThat(number).isEqualTo(9);
+    }
+    @Test
+    void shouldGetReceiveResultFromSingleCategoryForSixType(){
+        List<Integer> scoreOfThrows = Arrays.asList(6, 3, 3, 6, 4, 4);
+        String category = "Six";
+        int number = yahtzeeServer.getPoint(scoreOfThrows, category);
+        assertThat(number).isEqualTo(12);
     }
 
 }
